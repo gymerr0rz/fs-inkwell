c752ad29c16b72268b683db0e59c050668a92509
ERR0RZ <claimclaw@gmail.com>
2023-03-14 16:44:10 +0100

    changing js to jsx and adding notes pages

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -4,6 +4,7 @@ import LoginPage from './pages/auth/Login';
 import React from 'react';
 import RegisterPage from './pages/auth/Register';
 import MainPage from './pages/Main';
+import NotesPage from './pages/app/NotesPage';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<MainPage />} />
         <Route path="/auth/login" element={<LoginPage />} />
         <Route path="/auth/register" element={<RegisterPage />} />
+        <Route path="/app/notes" element={<NotesPage />} />
       </Routes>
     </>
   );
---
+++ front-end/src/pages/app/NotesPage.jsx
@@ -0,0 +1,5 @@
+const NotesPage = () => {
+  return <h1>Notes Page</h1>;
+};
+
+export default NotesPage;
