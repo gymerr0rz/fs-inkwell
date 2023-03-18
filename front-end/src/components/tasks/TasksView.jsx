a4f6d2ffc2c73c5bd1ba59648e80da219c606a56
ERR0RZ <claimclaw@gmail.com>
2023-03-18 17:23:36 +0100

    commit

--- front-end/src/components/tasks/TasksView.jsx
+++ front-end/src/components/tasks/TasksView.jsx
@@ -1,18 +1,51 @@
 import { List, Search } from 'lucide-react';
+import {
+  TasksContainer,
+  TasksTop,
+  Icon,
+  TasksSearch,
+  TasksBottom,
+  TasksTopText,
+} from '../../styles/tasks/TasksView.styled';
 
 const TasksView = () => {
   return (
-    <div>
-      <div>
-        <h1>My Tasks</h1>
-        <br />
-        <List />
-        <div>
+    <TasksContainer>
+      <TasksTop>
+        <TasksTopText>My Tasks</TasksTopText>
+        <Icon>
+          <List />
+          List
+        </Icon>
+        <TasksSearch>
           <Search />
-          <input type="text" />
+          <input type="text" placeholder="Search..." />
+        </TasksSearch>
+      </TasksTop>
+      <TasksBottom>
+        <div>
+          <div>
+            <h1>New Tasks</h1>
+            <div>3</div>
+            <button>ADD</button>
+          </div>
         </div>
-      </div>
-    </div>
+        <div>
+          <div>
+            <h1>New Tasks</h1>
+            <div>3</div>
+            <button>ADD</button>
+          </div>
+        </div>
+        <div>
+          <div>
+            <h1>New Tasks</h1>
+            <div>3</div>
+            <button>ADD</button>
+          </div>
+        </div>
+      </TasksBottom>
+    </TasksContainer>
   );
 };
 
---
+++ front-end/src/styles/tasks/TasksView.styled.js
@@ -0,0 +1,74 @@
+import styled from 'styled-components';
+
+const TasksContainer = styled.div`
+  height: 100%;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  background-color: 121215;
+`;
+
+const TasksTopText = styled.h1`
+  color: #fff;
+  font-size: 3rem;
+  white-space: nowrap;
+`;
+
+const TasksTop = styled.div`
+  border-bottom: #848486 1px solid;
+  display: flex;
+  justify-content: flex-start;
+  gap: 40px;
+  align-items: center;
+  width: 100%;
+  height: 20vh;
+  padding: 50px;
+  position: relative;
+`;
+
+const TasksSearch = styled.div`
+  width: 100%;
+  height: 60px;
+  background-color: #2b2c2e;
+  outline: none;
+  border: none;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 10px;
+  color: #fff;
+  gap: 10px;
+  border-radius: 10px;
+  input {
+    outline: none;
+    color: #fff;
+    background-color: #2b2c2e;
+    border: none;
+    height: 100%;
+    width: 100%;
+  }
+`;
+
+const Icon = styled.div`
+  cursor: pointer;
+  :hover {
+    color: #fff;
+  }
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  gap: 5px;
+  color: #848486;
+`;
+
+const TasksBottom = styled.div``;
+
+export {
+  TasksContainer,
+  TasksTop,
+  TasksSearch,
+  Icon,
+  TasksBottom,
+  TasksTopText,
+};
