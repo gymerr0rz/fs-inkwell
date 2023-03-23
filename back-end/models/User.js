05083a7f11592df8e90967fb055fe1549ab0e07c
ERR0RZ <claimclaw@gmail.com>
2023-03-23 21:23:03 +0100

    commit

--- back-end/models/User.js
+++ back-end/models/User.js
@@ -2,67 +2,80 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
+  // User's unique username for login
   username: {
     type: String,
     required: true,
     unique: true,
   },
+
+  // User's unique email for contact and notifications
   email: {
     type: String,
     required: true,
     unique: true,
   },
+
+  // User's encrypted password for security
   password: {
     type: String,
     required: true,
   },
+
+  // User's profile image URL for display
   profile_image: {
     type: String,
-    default:
-      'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn.onlinewebfonts.com%2Fsvg%2Fimg_110805.png&f=1&nofb=1&ipt=c9ab557c33cc15e347b0b50c63db4aff92088959cb54dbe111f6c013639b1ca2&ipo=images',
-    required: false,
+    default: 'https://cdn.onlinewebfonts.com/svg/img_110805.png',
   },
+
+  // Date when the user account was created
   created_at: {
     type: Date,
-    default: Date.now(),
-    required: false,
+    default: Date.now,
   },
+
+  // JWT tokens for user authentication and authorization
   jwt_token: [
     {
-      token: {
-        type: String,
-      },
-      expiresAt: {
-        type: Date,
-      },
+      // Encrypted token string
+      token: String,
+
+      // Expiration date of the token
+      expiresAt: Date,
     },
   ],
+
+  // User's personal notes for organization and productivity
   notes: [
     {
+      // Unique title of the note
       title: {
         type: String,
         unique: true,
       },
-      content: {
-        type: String,
-      },
+
+      // Content of the note
+      content: String,
     },
   ],
+
+  // User's tasks for scheduling and tracking
   tasks: [
     {
+      // Unique title of the task
       title: {
         type: String,
         unique: true,
       },
-      category: {
-        type: String,
-      },
-      date: {
-        type: String,
-      },
-      origin: {
-        type: String,
-      },
+
+      // Category of the task (optional)
+      category: String,
+
+      // Date when the task is due (optional)
+      date: String,
+
+      // Origin of the task (optional)
+      origin: String,
     },
   ],
 });
