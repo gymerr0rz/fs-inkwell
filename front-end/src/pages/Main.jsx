018629324c9a4744680162a81f7bc1ca7771862d
ERR0RZ <claimclaw@gmail.com>
2023-04-04 22:04:10 +0200

    commit

--- front-end/src/pages/Main.jsx
+++ front-end/src/pages/Main.jsx
@@ -1,23 +1,68 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { StyledButton } from '../styles/Buttons,styled';
-import { MainPageStyle } from '../styles/Main.styled';
+import {
+  MainPageStyle,
+  HomeNavbar,
+  Container,
+  Hero,
+  HeroTwo,
+  HeroButtons,
+  Flex,
+} from '../styles/Main.styled';
+import Book from '../assets/undraw_design_notes_re_eklr.svg';
 
 const MainPage = () => {
   return (
     <>
-      <MainPageStyle>
-        <h1>INKWELL</h1>
-        <p>Log in with your INKWELL account to continue.</p>
-        <div>
-          <Link to="/auth/register">
-            <StyledButton>Register</StyledButton>
-          </Link>
-          <Link to="/auth/login">
-            <StyledButton>Log in</StyledButton>
-          </Link>
-        </div>
-      </MainPageStyle>
+      <Container>
+        <MainPageStyle>
+          <HomeNavbar>
+            <h1>INKWELL</h1>
+            <ul>
+              <li>About us</li>
+              <li>Contact us</li>
+              <li>
+                <button>Log in</button>
+              </li>
+            </ul>
+          </HomeNavbar>
+          <Hero>
+            <h1>Unleashing your creativity, one idea at a time.</h1>
+            <p>
+              Social productivity that puts you back in control. Connect,
+              create, and collaborate with like-minded individuals in a safe and
+              private environment. Join us and experience a new kind of social
+              media.
+            </p>
+            <HeroButtons>
+              <button className="login">Log in</button>
+              <button className="register">Create account</button>
+            </HeroButtons>
+          </Hero>
+        </MainPageStyle>
+      </Container>
+
+      {/* Second Hero */}
+      <Container>
+        <MainPageStyle>
+          <HeroTwo>
+            <Flex>
+              <h1>Everything you need all in one application</h1>
+              <p>
+                Inkwell is your all-in-one productivity application. With
+                features like note-taking, task management, and the Pomodoro
+                technique, Inkwell simplifies your workflow and boosts your
+                productivity. Say goodbye to juggling multiple apps and hello to
+                a streamlined experience that helps you stay focused and achieve
+                your goals.
+              </p>
+              <button className="register">Create account</button>
+            </Flex>
+            <img src={Book} alt="" />
+          </HeroTwo>
+        </MainPageStyle>
+      </Container>
     </>
   );
 };
--- front-end/src/styles/Main.styled.js
+++ front-end/src/styles/Main.styled.js
@@ -1,26 +1,172 @@
 import styled from 'styled-components';
 
 const MainPageStyle = styled.div`
-  width: 100vw;
-  height: 100vh;
+  width: 80%;
+  margin: 0 auto;
+  height: 100%;
+  position: relative;
+  overflow: hidden;
+`;
+
+const HomeNavbar = styled.div`
+  position: fixed;
   display: flex;
-  justify-content: center;
-  flex-direction: column;
-  background-color: rgba(0, 0, 5, 0.95);
+  justify-content: space-between;
   align-items: center;
+  padding: 0 20px;
+  width: 80%;
+  height: 150px;
   color: #fff;
-  h1 {
-    margin: 10px;
-  }
-  p {
-    font-weight: 400;
-    word-spacing: 1px;
-  }
-  button {
-    text-transform: uppercase;
-    padding: 15px 40px;
-    margin: 10px 10px;
+  z-index: 99;
+  ul {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 20px;
+    list-style: none;
+    button {
+      background-color: #6364ff;
+      border: none;
+      color: #fff;
+      outline: none;
+      width: 104px;
+      height: 40px;
+      border-radius: 10px;
+      cursor: pointer;
+      :hover {
+        transition: 100ms background-color ease-in-out;
+        background-color: #5453d0;
+      }
+    }
   }
 `;
 
-export { MainPageStyle };
+const Hero = styled.div`
+  text-align: center;
+  color: #fff;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  width: 100%;
+  gap: 20px;
+  h1 {
+    font-size: 64px;
+    width: 950px;
+    margin: 0 auto;
+  }
+  p {
+    font-size: 24px;
+    font-weight: 300;
+    width: 650px;
+    margin: 0 auto;
+  }
+
+  .login {
+    width: 180px;
+    height: 60px;
+    background-color: #6364ff;
+    color: #fff;
+    border: none;
+    font-size: 1.5rem;
+    border-radius: 10px;
+    :hover {
+      transition: 100ms background-color ease-in-out;
+      background-color: #5453d0;
+    }
+  }
+
+  .register {
+    width: 270px;
+    height: 60px;
+    background-color: #fff;
+    color: #6364ff;
+    border: none;
+    font-size: 1.5rem;
+    border-radius: 10px;
+    :hover {
+      transition: 100ms background-color ease-in-out;
+      background-color: #d0d0d0;
+    }
+  }
+`;
+
+const HeroTwo = styled.div`
+  text-align: left;
+  color: #fff;
+  display: flex;
+  flex-direction: color;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  width: 100%;
+  gap: 20px;
+
+  img {
+    max-height: 670px;
+    max-width: 670px;
+  }
+
+  .register {
+    width: 270px;
+    height: 60px;
+    background-color: #6364ff;
+    color: #fff;
+    border: none;
+    font-size: 1.5rem;
+    border-radius: 10px;
+    cursor: pointer;
+    :hover {
+      transition: 100ms background-color ease-in-out;
+      background-color: #5453d0;
+    }
+  }
+`;
+
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+`;
+
+const HeroButtons = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  gap: 20px;
+  margin-top: 30px;
+
+  button {
+    cursor: pointer;
+    transition: 100ms background-color ease-in-out;
+  }
+`;
+
+const Flex = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  gap: 30px;
+  width: 100%;
+  h1 {
+    font-size: 48px;
+    width: 500px;
+  }
+  p {
+    font-size: 24px;
+    width: 600px;
+    font-weight: 300;
+  }
+`;
+
+export {
+  MainPageStyle,
+  HomeNavbar,
+  Container,
+  Hero,
+  HeroButtons,
+  HeroTwo,
+  Flex,
+};
